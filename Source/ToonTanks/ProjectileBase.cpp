3e66266e6425e48220083b1a72720b9672b939c8
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2020-07-15 00:29:59 -0300

    Delegate OnHit

--- Source/ToonTanks/ProjectileBase.cpp
+++ Source/ToonTanks/ProjectileBase.cpp
@@ -4,6 +4,7 @@
 #include "ProjectileBase.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -12,6 +13,7 @@ AProjectileBase::AProjectileBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
 	RootComponent = ProjectileMesh;
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
@@ -27,6 +29,27 @@ void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AProjectileBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	//Try to get a reference to the owning class
+	AActor* MyOwner = GetOwner();
+
+	if (!MyOwner)
+	{
+		return;
+	}
+
+	//if OtherActors isn't self ow owner && exists, then apply damage
+	if (OtherActor != NULL && OtherActor != this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner->GetInstigatorController(), this, DamageType);
+	}
+
+	// Do a bunch of effects here during polish game
+
+	Destroy();
 }
 
 
--- Source/ToonTanks/ProjectileBase.h
+++ Source/ToonTanks/ProjectileBase.h
@@ -38,4 +38,7 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage", meta = (AllowPrivateAccess = "true"))
 	float Damage = 50.0f;
 
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 };
